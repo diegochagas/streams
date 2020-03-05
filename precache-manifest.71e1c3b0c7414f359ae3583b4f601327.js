cc45f30ce0a04acd86213e67b126d7cc1d14476d
Diego Rocha Chagas <diego.rchagas@BRSPOBITANM0294.local>
2020-03-05 15:10:22 -0300

    Updates

---
+++ precache-manifest.71e1c3b0c7414f359ae3583b4f601327.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44e6761289d78e176fca400ed4ada424",
+    "url": "/streams/index.html"
+  },
+  {
+    "revision": "d49930d93646c5eb580f",
+    "url": "/streams/static/css/main.7835b375.chunk.css"
+  },
+  {
+    "revision": "883284a1a032daf6c293",
+    "url": "/streams/static/js/2.8fa057d5.chunk.js"
+  },
+  {
+    "revision": "dbe5ca7a82c728776bb536c3d2d64135",
+    "url": "/streams/static/js/2.8fa057d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d49930d93646c5eb580f",
+    "url": "/streams/static/js/main.8fb08bf1.chunk.js"
+  },
+  {
+    "revision": "49484a28f577815fbdc9",
+    "url": "/streams/static/js/runtime-main.0af8cd63.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/streams/static/media/logo.5d5d9eef.svg"
+  }
+]);
